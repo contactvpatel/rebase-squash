e7859d7353ad74a7766389a67f23569fa1923cd6
Vishal Patel <contactvpatel@gmail.com>
2025-02-19 13:56:18 -0500

    Test 2

--- src/SampleApp/Controllers/WeatherForecastController.cs
+++ src/SampleApp/Controllers/WeatherForecastController.cs
@@ -22,6 +22,7 @@ public class WeatherForecastController : ControllerBase
     public IEnumerable<WeatherForecast> Get()
     {
         _logger.LogInformation("Test");
+        _logger.LogInformation("Test2");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
